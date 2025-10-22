c5f139bf1006edf22ad099d887d80c7e2f18e7a9
Ben-dak <dominickfrench.oe@gmail.com>
2025-10-22 17:57:01 -0500

    Added getters and setters to Employee class (with comments).

--- src/main/java/com/pluralsight/Employee.java
+++ src/main/java/com/pluralsight/Employee.java
@@ -10,6 +10,86 @@ public class Employee {
     private int hoursWorked;
     private boolean atWork;
 
+    public Employee(int employeeId, String name, String department, double payRate, int hoursWorked, boolean atWork) {
+        this.employeeId = employeeId;
+        this.name = name;
+        this.department = department;
+        this.payRate = payRate;
+        this.hoursWorked = hoursWorked;
+        this.atWork = atWork;
+    }
+
+    // getters and setters
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(int employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public double getPayRate() {
+        return payRate;
+    }
+
+    public void setPayRate(double payRate) {
+        this.payRate = payRate;
+    }
+
+    public int getHoursWorked() {
+        return hoursWorked;
+    }
+
+    public void setHoursWorked(int hoursWorked) {
+        this.hoursWorked = hoursWorked;
+    }
+
+    public boolean isAtWork() {
+        return atWork;
+    }
+
+    public void setAtWork(boolean atWork) {
+        this.atWork = atWork;
+    }
+
+    public double getRegularHours() {
+        if (this.hoursWorked <= 40){
+            return this.hoursWorked;
+        }else {
+            return 40;
+        }
+    }
+
+    public double getOvertimeHours(){
+        if (this.hoursWorked > 40){
+            return this.hoursWorked - 40;
+        }else {
+            return 0;
+        }
+    }
+    // pay rate and hours worked
+    // whether they worked overtime or not
+    // regular pay (getRegularHours x payrate) + overtime pay (getRegularHours x payrate) + (overtime hours * payrate * 1.5)
+    public double getTotalPay() {
+        return (getRegularHours() * this.payRate) + (getOvertimeHours() * this.payRate * 1.5);
+    }
+
     public boolean punchIn() {
         if(this.atWork) {
             System.out.println("already at work, cant punch in.");
@@ -43,4 +123,5 @@ public class Employee {
         }
         return false;
     }
+
 }
--- src/main/java/com/pluralsight/Reservation.java
+++ src/main/java/com/pluralsight/Reservation.java
@@ -5,9 +5,14 @@ public class Reservation {
     private String roomType;
     private int numberOfNights;
     private boolean isWeekend;
+    private double price;
 
     //constructor
-    public Reservation(String roomType, double price, int numberOfNights, boolean isWeekend, double reservationTotal ) {
+    public Reservation(String roomType, int numberOfNights, boolean isWeekend, double price) {
+        this.roomType = roomType;
+        this.numberOfNights = numberOfNights;
+        this.isWeekend = isWeekend;
+        this.price = price;
     }
 
     public String getRoomType() {
@@ -22,22 +27,36 @@ public class Reservation {
         return this.isWeekend;
     }
 
+    public double getReservationTotal() {
+        return getPrice() * numberOfNights;
+    }
+
+    public double getPrice() {
+//        double basePrice;
+        if (getRoomType().equalsIgnoreCase("king")) {
+            setPrice(139.00);
+        } else {
+            setPrice(124.00);
+        }
+        return this.price;
+    }
+
     // setter
     public void setRoomType(String roomType) {
         this.roomType = roomType;
     }
 
-    public double getPrice() {
-        double basePrice;
-        if (roomType.equalsIgnoreCase("king")) {
-            basePrice = 139.00;
-        } else {
-            basePrice = 124.00;
-        }
-        return basePrice;
+    public void setNumberOfNights(int numberOfNights) {
+        this.numberOfNights = numberOfNights;
     }
 
-    public double getReservationTotal() {
-        return getPrice() * numberOfNights;
+    public void setWeekend(boolean weekend) {
+        this.isWeekend = weekend;
     }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+    // Methods
+
 }
