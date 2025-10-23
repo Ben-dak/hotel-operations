3d4ef90e187c6d4e000224c9cb80545602d1086f
Ben-dak <dominickfrench.oe@gmail.com>
2025-10-23 10:28:18 -0500

    Created RoomTest structure.

---
+++ src/test/java/com/pluralsight/RoomTest.java
@@ -0,0 +1,19 @@
+package com.pluralsight;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RoomTest {
+
+    @Test
+    void checkIn() {
+        Room room = new Room();
+
+        // Arrange
+
+        // Act
+
+        // Assert
+    }
+}
